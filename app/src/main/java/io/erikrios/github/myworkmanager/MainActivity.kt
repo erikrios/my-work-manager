04d613971bb982795977eef1ed4aad5d9aafacb3
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-13 20:25:40 +0700

    Implements the periodic task with task manager

--- app/src/main/java/io/erikrios/github/myworkmanager/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myworkmanager/MainActivity.kt
@@ -5,10 +5,12 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.*
 import io.erikrios.github.myworkmanager.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var periodicWorkRequest: PeriodicWorkRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +18,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
 
         binding.btnOneTimeTask.setOnClickListener(this)
+        binding.btnPeriodicTask.setOnClickListener(this)
+        binding.btnCancelTask.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             binding.btnOneTimeTask.id -> startOneTimeTask()
+            binding.btnPeriodicTask.id -> startPeriodicTask()
+            binding.btnCancelTask.id -> cancelPeriodicTask()
         }
     }
 
@@ -43,5 +49,36 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val status = workInfo.state.name
                 binding.textStatus.append("\n$status")
             })
+    }
+
+    private fun startPeriodicTask() {
+        binding.textStatus.text = getString(R.string.status)
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+            .build()
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        periodicWorkRequest =
+            PeriodicWorkRequest.Builder(MyWorker::class.java, 15, TimeUnit.MINUTES)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .build()
+        WorkManager.getInstance().enqueue(periodicWorkRequest)
+        WorkManager.getInstance()
+            .getWorkInfoByIdLiveData(periodicWorkRequest.id)
+            .observe(this@MainActivity, { workInfo ->
+                val status = workInfo.state.name
+                binding.textStatus.append("\n$status")
+                binding.btnCancelTask.isEnabled = false
+                if (workInfo.state == WorkInfo.State.ENQUEUED) {
+                    binding.btnCancelTask.isEnabled = true
+
+                }
+            })
+    }
+
+    private fun cancelPeriodicTask() {
+        WorkManager.getInstance().cancelWorkById(periodicWorkRequest.id)
     }
 }
