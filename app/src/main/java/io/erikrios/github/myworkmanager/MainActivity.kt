0be0d2705e2bbb6adbbf7d20c85983ad13f51114
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-13 19:25:36 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/myworkmanager/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myworkmanager/MainActivity.kt
@@ -2,10 +2,22 @@ package io.erikrios.github.myworkmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import io.erikrios.github.myworkmanager.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnOneTimeTask.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
     }
 }
